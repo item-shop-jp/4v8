2b06cc2060223acb07f300971c5af90fefa20bc2
katagiri <katagiri@item-shop.jp>
2021-10-29 14:37:57 +0900

    feat: add useMutationObserver

--- src/components/blocks/Container.tsx
+++ src/components/blocks/Container.tsx
@@ -6,53 +6,53 @@ import { useBlockRenderer } from '../../hooks/use-block-renderer';
 import { InlineContent } from '../../utils/inline';
 
 interface BlockProps {
-    blockId: string;
-    readOnly: boolean;
-    formats: Formats;
-    editor: EditorController;
-    onClick: (e: React.MouseEvent) => void;
-    onKeyDown: (e: React.KeyboardEvent) => void;
-    onCompositionStart: (e: React.CompositionEvent) => void;
-    onCompositionEnd: (e: React.CompositionEvent) => void;
-    onBeforeInput: (e: React.KeyboardEvent) => void;
-  }
-  
-  export const BlockContainer: React.VFC<BlockProps> = React.memo(
-    ({ blockId, editor, readOnly = false, formats, ...props }) => {
-      const [blockFormat, setBlockFormat] = React.useState<string>();
-      const [Container, setContainer] = React.useState<React.FC<any>>(formats['block/text']);
-      const block = useBlockRenderer({ blockId, editor });
+  blockId: string;
+  readOnly: boolean;
+  formats: Formats;
+  editor: EditorController;
+  onClick: (e: React.MouseEvent) => void;
+  onKeyDown: (e: React.KeyboardEvent) => void;
+  onCompositionStart: (e: React.CompositionEvent) => void;
+  onCompositionEnd: (e: React.CompositionEvent) => void;
+  onBeforeInput: (e: React.KeyboardEvent) => void;
+}
 
-      const memoContents = React.useMemo(() => {
-        return InlineContent({ contents: block?.contents ?? [], formats });
-      }, [block?.contents, formats]);
+export const BlockContainer: React.VFC<BlockProps> = React.memo(
+  ({ blockId, editor, readOnly = false, formats, ...props }) => {
+    const [blockFormat, setBlockFormat] = React.useState<string>();
+    const [Container, setContainer] = React.useState<React.FC<any>>(formats['block/text']);
+    const block = useBlockRenderer({ blockId, editor });
 
-      React.useEffect(() => {
-        const newBlockFormat = `block/${block?.type.toLocaleLowerCase()}`;
-        setBlockFormat(newBlockFormat);
-        setContainer(() => {
-          if (!formats[newBlockFormat]) {
-            // defalut block format
-            return formats['block/text'];
-          } else {
-            return formats[newBlockFormat];
-          }
-        });
-      }, [block?.type]);
-  
-      return (
-        <Container
-          suppressContentEditableWarning
-          className="notranslate"
-          contentEditable={!readOnly}
-          blockId={blockId}
-          data-block-id={blockId}
-          data-format={blockFormat}
-          formats={formats}
-          attributes={block?.attributes}
-          contents={memoContents}
-          {...props}
-        />
-      );
-    },
-  );
+    const memoContents = React.useMemo(() => {
+      return InlineContent({ contents: block?.contents ?? [], formats });
+    }, [block?.contents, formats]);
+
+    React.useEffect(() => {
+      const newBlockFormat = `block/${block?.type.toLocaleLowerCase()}`;
+      setBlockFormat(newBlockFormat);
+      setContainer(() => {
+        if (!formats[newBlockFormat]) {
+          // defalut block format
+          return formats['block/text'];
+        } else {
+          return formats[newBlockFormat];
+        }
+      });
+    }, [block?.type]);
+
+    return (
+      <Container
+        suppressContentEditableWarning
+        className="notranslate"
+        contentEditable={!readOnly}
+        blockId={blockId}
+        data-block-id={blockId}
+        data-format={blockFormat}
+        formats={formats}
+        attributes={block?.attributes}
+        contents={memoContents}
+        {...props}
+      />
+    );
+  },
+);
--- src/components/blocks/Header.tsx
+++ src/components/blocks/Header.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { Formats } from '../../types/format';
 import { EditorController } from '../../hooks/use-editor';
+import { Formats } from '../../types/format';
 import { Inline } from '../../types/inline';
 import { BlockAttributes } from '../../types/block';
 
@@ -9,6 +9,7 @@ interface Props {
   blockId: string;
   formats?: Formats;
   contents: Inline[];
+  length: number;
   attributes: BlockAttributes;
   editor: EditorController;
   onClick: (e: React.MouseEvent) => void;
@@ -17,6 +18,11 @@ interface Props {
 const Header1 = styled.h1`
   font-size: 24px;
   outline: 0;
+  transition: all 0.3s, color 0.3s;
+  ::after {
+    opacity: 0.3;
+    content: attr(placeholder);
+  }
 `;
 
 const Header2 = styled.h2`
@@ -29,31 +35,19 @@ const Header3 = styled.h3`
   outline: 0;
 `;
 
-export const Header = React.memo(({ contents, attributes, ...props }: Props) => {
+export const Header = React.memo(({ blockId, length, contents, attributes, editor, ...props }: Props) => {
   switch (attributes['header']) {
     case 1:
       return (
-        <Header1 {...props}>
+        <Header1 {...props} placeholder={length < 1 ? 'Heading 1' : ''}>
           {contents}
         </Header1>
       );
     case 2:
-      return (
-        <Header2 {...props}>
-          {contents}
-        </Header2>
-      );
+      return <Header2 {...props}>{contents}</Header2>;
     case 3:
-      return (
-        <Header3 {...props}>
-          {contents}
-        </Header3>
-      );
+      return <Header3 {...props}>{contents}</Header3>;
     default:
-      return (
-        <Header1 {...props}>
-          {contents}
-        </Header1>
-      );
+      return <Header1 {...props}>{contents}</Header1>;
   }
 });

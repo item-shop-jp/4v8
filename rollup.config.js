2dac3e0fb085b3996954c5929f1dec831cdfd73d
katagiri <katagiri@item-shop.jp>
2022-09-01 15:20:10 +0900

    build: ver 0.2.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
+import typescript from 'rollup-plugin-typescript2';
 
 const packageJson = require('./package.json');
 

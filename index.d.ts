6fe8d42f063d8a85c98919928abbdd5f2ec50ea6
girigiritech <katagiri@item-shop.jp>
2022-11-16 05:20:50 +0900

    update d.ts

--- index.d.ts
+++ index.d.ts
@@ -157,6 +157,40 @@ export interface CaretPosition {
   rect: DOMRect;
 }
 
+export interface JSON0 {
+  p: [keyof Block, number, keyof Inline, number];
+  li: any;
+  ld: any;
+  si: string;
+  sd: string;
+}
+
+export interface UpdateOp {
+  type: 'update_contents';
+  blockId: string;
+  undo: JSON0[];
+  redo: JSON0[];
+  position?: CaretPosition;
+}
+
+export interface AddOp {
+  type: 'add_block';
+  blockId: string;
+  block: Block;
+  prevBlockId?: string;
+  position?: CaretPosition;
+}
+
+export interface RemoveOp {
+  type: 'remove_block';
+  blockId: string;
+  block: Block;
+  prevBlockId?: string;
+  position?: CaretPosition;
+}
+
+export type Op = UpdateOp | AddOp | RemoveOp;
+
 export interface EditorEvent {
   key: string;
   data: any;

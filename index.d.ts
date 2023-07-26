ac984b00a640cb6c4b6edb65dd8d6be33ade0b19
katagiri <katagiri@item-shop.jp>
2023-07-26 12:51:41 +0900

    型情報を更新

--- index.d.ts
+++ index.d.ts
@@ -11,6 +11,7 @@ import {
   ToolbarModule,
   UploaderModule,
   KeyBoardModule,
+  TocModule,
 } from './src/packages/shibuya-editor/modules';
 import { FlattenSimpleInterpolation } from 'styled-components';
 
@@ -292,6 +293,7 @@ export interface EditorController {
   getModule(name: 'uploader'): UploaderModule;
   getModule(name: 'drag-drop'): DragDropModule;
   getModule(name: 'collaborator'): CollaboratorModule;
+  getModule(name: 'toc'): TocModule;
   getModule<T = any>(name: string): T | null;
   removeAllModules(): void;
   getEventEmitter(): EventEmitter;
